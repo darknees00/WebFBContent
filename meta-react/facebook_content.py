fe076a67269ffd777daf87510b327f8971a98999
darkness00 <thangthienk6@gmail.com>
2025-02-26 09:17:04 +0800

    port

--- meta-react/facebook_content.py
+++ meta-react/facebook_content.py
@@ -392,4 +392,4 @@ if __name__ == '__main__':
     scheduler_thread = Thread(target=run_scheduler)
     scheduler_thread.daemon = True
     scheduler_thread.start()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8084, debug=True)
